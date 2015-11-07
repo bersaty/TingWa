69ed2dcc0bf4d6b92260d108528f83abec17dd90
吴春辉 <wuchunhui@meizu.com>
2015-11-07 18:30:14 +0800

    modify

--- app/src/main/java/com/example/wuchunhui/tingwa/MainActivity.java
+++ app/src/main/java/com/example/wuchunhui/tingwa/MainActivity.java
@@ -6,25 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import java.util.List;
-
 import retrofit.Call;
 import retrofit.Retrofit;
 import retrofit.http.GET;
-import retrofit.http.Path;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView mTextView;
-    private Button mBtnVolley,mBtnRetrofit;
+    private Button mBtnRetrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,46 +22,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.text);
         mBtnRetrofit = (Button) findViewById(R.id.retofit);
-        mBtnVolley = (Button) findViewById(R.id.volley);
 
         mBtnRetrofit.setOnClickListener(this);
-        mBtnVolley.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.volley:
-                VolleyTest();
-                break;
             case R.id.retofit:
                 RetrofitTest();
                 break;
         }
-    }
-
-    private void VolleyTest(){
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://www.itingwa.com";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: " + response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
     }
 
     private void RetrofitTest(){
@@ -80,11 +42,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .baseUrl("http://www.itingwa.com/")
 //                .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
+        TingWaValue service = retrofit.create(TingWaValue.class);
+
+        Call<String> str = service.listRepos();
+        mTextView.setText(str.toString());
+
     }
 
-    public interface GitHubService {
-        @GET("/users/{user}/repos")
-        Call<List<Response>> listRepos(@Path("user") String user);
+    public interface TingWaValue {
+        @GET("/u/103559")
+        Call<String> listRepos();
     }
 
 }
